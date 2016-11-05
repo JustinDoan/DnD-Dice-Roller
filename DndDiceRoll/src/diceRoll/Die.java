abd79f9cec406e1aa317638d9ef1faf2be95f405
Justin Doan <JustinDoan@mail.com>
2016-11-05 10:39:21 -0400

    Added:
    New Creation Tab for Dice
    
    Removed Resize Function
    
    Setup class for dice creation
    
    To be added:
    
    Dice notation intake system
    
    Putting in toggles and switches for dice creation.
    
    Planning out where things will be placed on the window

---
+++ DndDiceRoll/src/diceRoll/Die.java
@@ -0,0 +1,28 @@
+package diceRoll;
+
+public class Die {
+	private int number;
+	
+	
+	public Die(int number){
+		this.setNumber(number);
+	}
+
+
+	public int getNumber() {
+		return number;
+	}
+
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
