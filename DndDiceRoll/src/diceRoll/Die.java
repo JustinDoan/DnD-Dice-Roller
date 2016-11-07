e4f38474659d0a33af00f275f7b6b169c2f53e13
Justin Doan <JustinDoan@mail.com>
2016-11-07 07:58:06 -0500

    Added more Tab features
    put in custom dice
    
    put places for dice to go
    
    put in closers for all of the die
    
    alot of stuff mostly

--- DndDiceRoll/src/diceRoll/Die.java
+++ DndDiceRoll/src/diceRoll/Die.java
@@ -2,10 +2,15 @@ package diceRoll;
 
 public class Die {
 	private int number;
+	private String dieName;
+	private String dieLabel;
+	private int rolls;
 	
-	
-	public Die(int number){
-		this.setNumber(number);
+	public Die(int number, String dieName, String dieLabel, int rolls){
+		this.number = number;
+		this.setDieName(dieName);
+		this.dieLabel = dieLabel;
+		this.setRolls(rolls);
 	}
 
 
@@ -17,6 +22,41 @@ public class Die {
 	public void setNumber(int number) {
 		this.number = number;
 	}
+
+
+	public String getDieName() {
+		return dieName;
+	}
+
+
+	public void setDieName(String dieName) {
+		this.dieName = dieName;
+	}
+
+
+	public String getDieLabel() {
+		return dieLabel;
+	}
+
+
+	public void setDieLabel(String dieLabel) {
+		this.dieLabel = dieLabel;
+	}
+
+
+	public int getRolls() {
+		return rolls;
+	}
+
+
+	public void setRolls(int rolls) {
+		this.rolls = rolls;
+	}
+
+
+
+
+
 	
 	
 	
--- DndDiceRoll/src/diceRoll/WindowController.java
+++ DndDiceRoll/src/diceRoll/WindowController.java
@@ -1,7 +1,11 @@
 package diceRoll;
 
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import javafx.animation.KeyFrame;
@@ -9,9 +13,17 @@ import javafx.animation.KeyValue;
 import javafx.animation.ScaleTransition;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
@@ -23,6 +35,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Popup;
 import javafx.stage.Window;
 import javafx.util.Duration;
+import diceRoll.Die;
 
 public class WindowController {
 	Random rand = new Random();
@@ -33,8 +46,11 @@ public class WindowController {
 	public Image screencap;
 	private Popup moveTrackingPopup;
 	public int dieNumber = 1;
-	
-	
+	boolean choiceBoxFlag;
+	public int dieIdNumber = 0;
+	public List<Die> CreatedDies = new ArrayList<Die>();
+	boolean customDieFlag = false;
+	public String dieLocations[] = new String[6];
 	
 	@FXML
     private VBox window;
@@ -62,7 +78,215 @@ public class WindowController {
     
     @FXML
     private HBox HbDiceContainer;
+    @FXML
+    private ChoiceBox<String> dieTypeSelection;
+    
+    @FXML
+    private Button submitDie;
+    
+    //Took way too long to realize that I could just init all of these in one line.
+    @FXML
+    private Label diceLabel1,diceLabel2,diceLabel3,diceLabel4,diceLabel5,diceLabel6;
 
+    @FXML
+    private Label customDieLabel;
+
+    @FXML
+    private TextField customDieNumber;
+    
+    @FXML
+    private TextField rollNumber;
+    
+    @FXML
+    private Label rollsDice1,rollsDice2,rollsDice3,rollsDice4,rollsDice5,rollsDice6;
+    
+    @FXML
+    private Label customSides1,customSides2,customSides3,customSides4,customSides5,customSides6;
+       
+    @FXML
+    private ImageView exit1,exit2,exit3,exit4,exit5,exit6;
+    
+    @FXML
+    void initialize() {
+    	customDieNumber.setText(""); 
+    	dieTypeSelection.getItems().add("Four-Sided Die");
+    	dieTypeSelection.getItems().add("Six-Sided Die");
+    	dieTypeSelection.getItems().add("Eight-Sided Die");
+    	dieTypeSelection.getItems().add("Ten-Sided Die");
+    	dieTypeSelection.getItems().add("Tweleve-Sided Die");
+    	dieTypeSelection.getItems().add("Twenty-Sided Die");
+    	dieTypeSelection.getItems().add("Custom Die");
+    	
+    	dieTypeSelection.getSelectionModel().select(1);
+    	dieTypeSelection.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+    	      @Override
+    	      public void changed(ObservableValue<? extends String> observableValue, String oldString, String newString) {
+    	    	  
+    	    	  if (newString == "Custom Die"){
+    	      		customDieLabel.setVisible(true);
+    	      		customDieNumber.setVisible(true);
+    	      		
+    	      		customDieFlag = true;
+    	      	  }else{
+    	      		customDieLabel.setVisible(false);
+    	      		customDieNumber.setVisible(false);
+    	      		
+    	      		customDieFlag = false;
+    	      	}
+    	      }
+    	    });
+    	
+    }
+    
+
+    @FXML
+    void createDie(ActionEvent event) {
+    	int custom = 1;
+    	int rolls = 1;
+    	if (customDieFlag == false){
+    		
+		}else{
+			try {
+				custom = Integer.parseInt(customDieNumber.getText());
+			} catch (NumberFormatException e) {
+				System.out.println("Not a valid Custom Number");
+				return;
+			}
+		}
+    	
+    	try {
+			rolls = Integer.parseInt(rollNumber.getText());
+		} catch (NumberFormatException e) {
+			System.out.println("Rolls Defaulted to 1");
+			rolls = 1;
+		}
+    	
+    	
+    	
+    	//This will create a unique id for each created die
+    	String dieName = "Die" + Integer.toString(dieIdNumber);
+    	String selection = dieTypeSelection.getValue().toString();
+    	
+    	
+    	switch (selection){
+	    	case "Four-Sided Die":
+	    		Die die = new Die(4,dieName,selection,rolls);
+	    		CreatedDies.add(die);
+	    		break;
+	    		
+	    	case "Six-Sided Die":
+	    		Die die6 = new Die(6,dieName,selection,rolls);
+	    		CreatedDies.add(die6);
+	    		break;
+	    		
+	    	case "Eight-Sided Die":
+	    		Die die8 = new Die(8,dieName,selection,rolls);
+	    		CreatedDies.add(die8);
+	    		break;
+	    		
+	    	case "Ten-Sided Die":
+	    		Die die10 = new Die(10,dieName,selection,rolls);
+	    		CreatedDies.add(die10);
+	    		break;
+	    		
+	    	case "Tweleve-Sided Die":
+	    		Die die12 = new Die(12,dieName,selection,rolls);
+	    		CreatedDies.add(die12);
+	    		break;
+	    		
+	    	case "Twenty-Sided Die":
+	    		Die die20 = new Die(20,dieName,selection,rolls);
+	    		CreatedDies.add(die20);
+	    		break;
+	    		
+	    	case "Custom Die":
+	    		Die dieC = new Die(custom,dieName,selection,rolls);
+	    		CreatedDies.add(dieC);
+	    		break;
+    	}
+    	
+    	
+    	
+    	if (diceLabel1.getText() == ""){
+    		//if Dice is a Custom dice, add in Custom sides amount
+    		if (selection == "Custom Die"){
+    			customSides1.setText((CreatedDies.get(dieIdNumber).getNumber()) + " sides");
+    			//Otherwise print normal text
+    		}
+    		diceLabel1.setText((CreatedDies.get(dieIdNumber).getDieLabel()));
+    		 //This adds the amount of rolls
+    		rollsDice1.setText(Integer.toString(CreatedDies.get(dieIdNumber).getRolls()) + " Roll(s)");
+    		//This sets the DieIDNumber to a location in an array. Allowing us to manage where Dice are 
+    		//Located on the dice rolling screen
+    		dieLocations[0] = dieName;
+    	}else if(diceLabel2.getText() == ""){
+    		
+    		if (selection == "Custom Die"){
+    			customSides2.setText((CreatedDies.get(dieIdNumber).getNumber()) + " sides");
+    		}
+    		
+    		diceLabel2.setText((CreatedDies.get(dieIdNumber).getDieLabel()));
+    	
+    		rollsDice2.setText(Integer.toString(CreatedDies.get(dieIdNumber).getRolls()) + " Roll(s)");
+    		
+    		dieLocations[1] = dieName;
+    	}else if(diceLabel3.getText() == ""){
+    		
+    		if (selection == "Custom Die"){
+    			customSides3.setText((CreatedDies.get(dieIdNumber).getNumber()) + " sides");
+    		}
+    		
+    		diceLabel3.setText((CreatedDies.get(dieIdNumber).getDieLabel()));
+    	
+    		rollsDice3.setText(Integer.toString(CreatedDies.get(dieIdNumber).getRolls()) + " Roll(s)");
+    		
+    		dieLocations[2] = dieName;
+    	}else if(diceLabel4.getText() == ""){
+    		
+    		if (selection == "Custom Die"){
+    			customSides4.setText((CreatedDies.get(dieIdNumber).getNumber()) + " sides");
+    		}
+    		
+    		diceLabel4.setText((CreatedDies.get(dieIdNumber).getDieLabel()));
+    	
+    		rollsDice4.setText(Integer.toString(CreatedDies.get(dieIdNumber).getRolls()) + " Roll(s)");
+    		
+    		dieLocations[3] = dieName;
+    	}else if(diceLabel5.getText() == ""){
+    		
+    		if (selection == "Custom Die"){
+    			customSides5.setText((CreatedDies.get(dieIdNumber).getNumber()) + " sides");
+    		}
+    		
+    		diceLabel5.setText((CreatedDies.get(dieIdNumber).getDieLabel()));
+    		
+    	
+    		rollsDice5.setText(Integer.toString(CreatedDies.get(dieIdNumber).getRolls()) + " Roll(s)");
+    		
+    		dieLocations[4] = dieName;
+    	}else if(diceLabel6.getText() == ""){
+    		
+    		if (selection == "Custom Die"){
+    			customSides6.setText((CreatedDies.get(dieIdNumber).getNumber()) + " sides");
+    		}
+    		
+    		diceLabel6.setText((CreatedDies.get(dieIdNumber).getDieLabel()));
+    	
+    		rollsDice6.setText(Integer.toString(CreatedDies.get(dieIdNumber).getRolls()) + " Roll(s)");
+    		
+    		dieLocations[5] = dieName;
+    	}else{
+    		System.out.println("Nowhere to go!");
+    		CreatedDies.remove(dieIdNumber);
+    		return;
+    	}
+    	dieIdNumber= dieIdNumber + 1;
+    }
+    
+    
+    
+    
+    
     
     @FXML
     void closeWindow(MouseEvent event) {
@@ -176,8 +400,8 @@ public class WindowController {
         
     }
     @FXML
-    void openOrCloseTab(MouseEvent event) {
-    	
+    public void openOrCloseTab(MouseEvent event) {
+    	choiceBoxFlag = dieTypeSelection.isVisible();
     	
     	tabAnimatedResizeVbTabContent(VbTabContent);
     	tabAnimatedResizeHBox(HbTabTitle);
@@ -188,21 +412,51 @@ public class WindowController {
     		HbDiceContainer.setPrefWidth(380);
     	}*/
     	
+    	if ( choiceBoxFlag == true){
+    		dieTypeSelection.setVisible(false);
+    		submitDie.setVisible(false);
+    		rollNumber.setVisible(false);
+    	}else{
+    		dieTypeSelection.setVisible(true);
+    		submitDie.setVisible(true);
+    		rollNumber.setVisible(true);
+    	}
+    	
+    	
+    	
+    	if (choiceBoxFlag == true){
+    		
+    		customDieLabel.setVisible(false);
+    		customDieNumber.setVisible(false);
+    		
+    	}else{
+    		
+    		if (customDieFlag == false){
+    			
+    			customDieLabel.setVisible(false);
+        		customDieNumber.setVisible(false);
+    		}else{
+    			
+    			customDieLabel.setVisible(true);
+        		customDieNumber.setVisible(true);
+    		}
+    	}
     }
     
     public void tabResizeHbDiceContentResize(HBox DiceContainer) {
     	int width = (int) DiceContainer.getWidth();
-    	if (width == 380){
+    	
+    	if (width == 399){
     		Timeline timeline = new Timeline();
-    				timeline.getKeyFrames().add(new KeyFrame(Duration.ZERO, new KeyValue(DiceContainer.prefWidthProperty(),380)));
-                    timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(.6), new KeyValue(DiceContainer.prefWidthProperty(),700)));
+    		timeline.getKeyFrames().add(new KeyFrame(Duration.ZERO, new KeyValue(DiceContainer.prefWidthProperty(),399)));
+            timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(.6), new KeyValue(DiceContainer.prefWidthProperty(),800)));
 
                 
     		timeline.play();
-    	}else{
+    	}else if(width > 399){
     		Timeline timeline = new Timeline();
-    		timeline.getKeyFrames().add(new KeyFrame(Duration.ZERO, new KeyValue(DiceContainer.prefWidthProperty(),700)));
-            timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(.6), new KeyValue(DiceContainer.prefWidthProperty(),380)));
+    		timeline.getKeyFrames().add(new KeyFrame(Duration.ZERO, new KeyValue(DiceContainer.prefWidthProperty(),800)));
+            timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(.6), new KeyValue(DiceContainer.prefWidthProperty(),399)));
 
                 
     		timeline.play();
@@ -251,7 +505,54 @@ public class WindowController {
     		
     	}
     
-    
+    //This block of methods is not good. There is def a better way to do this and I'm sorry for this.
+    @FXML
+    void destroyDie1(MouseEvent event) {
+    	diceLabel1.setText("");
+    	customSides1.setText("");
+    	rollsDice1.setText("");
+    	dieLocations[0] = null;
+    }
+
+    @FXML
+    void destroyDie2(MouseEvent event) {
+    	diceLabel2.setText("");
+    	customSides2.setText("");
+    	rollsDice2.setText("");
+    	dieLocations[1] = null;
+    }
+
+    @FXML
+    void destroyDie3(MouseEvent event) {
+    	diceLabel3.setText("");
+    	customSides3.setText("");
+    	rollsDice3.setText("");
+    	dieLocations[2] = null;
+    }
+
+    @FXML
+    void destroyDie4(MouseEvent event) {
+    	diceLabel4.setText("");
+    	customSides4.setText("");
+    	rollsDice4.setText("");
+    	dieLocations[3] = null;
+    }
+
+    @FXML
+    void destroyDie5(MouseEvent event) {
+    	diceLabel5.setText("");
+    	customSides5.setText("");
+    	rollsDice5.setText("");
+    	dieLocations[4] = null;
+    }
+
+    @FXML
+    void destroyDie6(MouseEvent event) {
+    	diceLabel6.setText("");
+    	customSides6.setText("");
+    	rollsDice6.setText("");
+    	dieLocations[5] = null;
+    }
     
     
     
